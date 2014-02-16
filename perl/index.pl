61b26058c97c031fe64150d5ff462d471dcd1d78
Tony-YI <455054421@qq.com>
2014-02-16 12:35:00 +0800

    reinit.html and reinit.cgi

--- perl/index.pl
+++ perl/index.pl
@@ -2,15 +2,17 @@
 
 use strict;
 use CGI;	#load the CGI module
+use CGI::Carp qw(warningsToBroswer fatalsToBrowser);
 
 my $CGI_o = CGI->new;
 
-print $CGI_o->header;
+print $CGI_o->header();
 
 print <<__CONTENT__	#here document
 <html>
 <body>
 Hello World!
+<a href="reinit.html">Re-Initialization</a>
 </body>
 </html>
 __CONTENT__
