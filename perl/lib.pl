baa493c095400f0e74b848aa0a7dab005952b81b
Tony-YI <455054421@qq.com>
2014-02-16 19:38:09 +0800

    reinit.html and reinit.cgi

--- perl/lib.pl
+++ perl/lib.pl
@@ -62,13 +62,13 @@ sub db_execute	#usage: query($query, \@result), parameter ($query) is the SQL qu
 sub db_create_table   #create all tables we need
 {
     db_connect();
-    my $query = "CREATE TABLE user (user_name CHAR(20), pass_word CHAR(20));";
+    my $query = "CREATE TABLE user (user_name CHAR(20) PRIMARY KEY, pass_word CHAR(20));";
     db_execute($query);
     
-    $query = "CREATE TABLE session (user_name CHAR(20), session_id CHAR(20));";
+    $query = "CREATE TABLE session (user_name CHAR(20) PRIMARY KEY, session_id CHAR(20));";
     db_execute($query);
     
-    $query = "CREATE TABLE file (user_name CHAR(20), file_name CHAR(20), file_size INT, upload_time INT, img_description CHAR(50), img_path CHAR(50), shortcut_path CHAR(50));";
+    $query = "CREATE TABLE file (user_name CHAR(20) PRIMARY KEY, file_name CHAR(20), file_size INT, upload_time INT, img_description CHAR(50), img_path CHAR(50), shortcut_path CHAR(50));";
     db_execute($query);
     db_disconnect();
 }
