9a6fcf93170aeaeb21f83a66d24bfe933f23d2aa
Tony-YI <455054421@qq.com>
2014-02-16 15:45:35 +0800

    reinit.html and reinit.cgi

--- perl/lib.pl
+++ perl/lib.pl
@@ -5,7 +5,7 @@
 
 use DBI;	#use DataBase Interface
 use CGI;
-use CGI::Carp qw(wariningsToBrowser fatalsToBrowser);
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 use strick;
 
 ###################################################
