8f68929e492f6be274f41a5726f00d4c31c11ff6
Tony-YI <455054421@qq.com>
2014-02-17 01:05:30 +0800

    testing

--- perl/lib.pl
+++ perl/lib.pl
@@ -163,6 +163,9 @@ sub create_dir  #if the ./user_name_img and ./user_name_shortcut do not exist, c
         #create dir
     }
     else{}
+    
+    my $out3 = `cd "$data_dir" && ls -a`;
+    print "<h4>$out3</h4></br>";
 }
 
 return 1;	#for header file, it must return 1, otherwise perl will exit with default value 0
