2593357181f05bcb184ad220cd8c7e187fa255ab
Tony-YI <455054421@qq.com>
2014-02-16 15:42:24 +0800

    reinit.html and reinit.cgi

--- perl/lib.pl
+++ perl/lib.pl
@@ -12,10 +12,10 @@ use strick;
 ###	Setup connection to MySQL databast	###
 ###################################################
 
-my $db_host = $ENV{"OPENSHIFT_MYSQL_DB_HOST"};
-my $db_username = $ENV{"OPENSHIFT_MYSQL_DB_USERNAME"};
-my $db_password = $ENV{"OPENSHIFT_MYSQL_DB_PASSWORD"};
-my $db_name = $ENV{"OPENSHIFT_APP_NAME"};	#default database name is same as the application name
+my $db_host = $ENV{'OPENSHIFT_MYSQL_DB_HOST'};
+my $db_username = $ENV{'OPENSHIFT_MYSQL_DB_USERNAME'};
+my $db_password = $ENV{'OPENSHIFT_MYSQL_DB_PASSWORD'};
+my $db_name = $ENV{'OPENSHIFT_APP_NAME'};	#default database name is same as the application name
 
 sub db_connect()	#pass by reference, parameter is (/$db_handler)
 {
