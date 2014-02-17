9c827e04fb3a915f96d8b5b34fed5e3f6263e5ee
Tony-YI <455054421@qq.com>
2014-02-17 15:44:35 +0800

    testing

--- perl/lib.pl
+++ perl/lib.pl
@@ -160,7 +160,7 @@ sub upload_pic  #if the ./user_name_img and ./user_name_shortcut do not exist, c
     my $description = shift @_;
     my $flag_ptr = shift @_;
     
-    if(!(-d "$upload_dir$user_name$img_dir") || !(-d "$upload_dir$user_name$shortcut_dir")) #dir not found
+    if((!(-d "$upload_dir$user_name$img_dir")) || (!(-d "$upload_dir$user_name$shortcut_dir"))) #dir not found
     {
         #create dir
         `cd "$upload_dir" && mkdir "$user_name$img_dir && mkdir "$user_name$shortcut_dir"`;
