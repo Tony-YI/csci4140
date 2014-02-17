fc2a303c9bbd2a95282ccfb10fdf5cb05c59c4c9
Tony-YI <455054421@qq.com>
2014-02-17 21:35:53 +0800

    testing

--- perl/lib.pl
+++ perl/lib.pl
@@ -160,6 +160,8 @@ sub upload_pic  #if the ./user_name_img and ./user_name_shortcut do not exist, c
     my $description = shift @_;
     my $flag_ptr = shift @_;
     
+    print "`$CGI_o_ptr` `$user_name` `$file_name` `$description` `flag_ptr`";
+    
     if(!(-d "$upload_dir$user_name$img_dir")) #dir not found
     {
         #create dir
@@ -224,8 +226,7 @@ sub upload_pic  #if the ./user_name_img and ./user_name_shortcut do not exist, c
     
     #upload description and other attributes to the database
     #my $query = "INSERT INTO file (user_name, file_name, file_size, upload_time, img_description, img_path, shortcut_path) VALUES ($user_name, $file_name, $totalBytes, 0, $description, $upload_dir$user_name$img_dir/$file_name, $upload_dir$user_name$img_dir/$file_name);";
-    my $query = "INSERT INTO file (user_name, file_name, file_size, upload_time, img_description, img_path, shortcut_path) VALUES ($user_name, $file_name, $totalBytes, 0, $description, $file_name, $file_name);";
-    db_execute($query);
+    #db_execute($query);
     
     $$flag_ptr = 1;
 }
