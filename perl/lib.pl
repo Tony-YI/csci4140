ae73345fe004e27d26f8ec8a6f374ba7ced6ee5d
Tony-YI <455054421@qq.com>
2014-02-17 15:52:25 +0800

    testing

--- perl/lib.pl
+++ perl/lib.pl
@@ -176,10 +176,6 @@ sub upload_pic  #if the ./user_name_img and ./user_name_shortcut do not exist, c
     #file not exist, upload picture
     if(!open(OUTFILE, "> $upload_dir$user_name$img_dir/$file_name"))    #can't open file for writing
     {
-        print "<h4>$user_name, $file_name</h4><br/>";
-        my $out3 = `cd "$upload_dir" && ls -a`;
-        print "<h4>$out3</h4></br>";
-        
         $$flag_ptr = 4;
         return;
     }
@@ -198,9 +194,18 @@ sub upload_pic  #if the ./user_name_img and ./user_name_shortcut do not exist, c
         $totalBytes += $ret;
         if($totalBytes > 1024*1024) #1 MB
         {
+            print "<h4>$user_name, $file_name</h4><br/>";
+            my $out3 = `cd "$upload_dir" && ls -a`;
+            print "<h4>$out3</h4></br>";
+            
             close(OUTFILE);
             `rm "$upload_dir$user_name$img_dir/$file_name"`;
             $$flag_ptr = 3;
+            
+            print "<h4>$user_name, $file_name</h4><br/>";
+            my $out3 = `cd "$upload_dir" && ls -a`;
+            print "<h4>$out3</h4></br>";
+            
             return;
         }
     }
