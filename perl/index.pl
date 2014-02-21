db12f9aa573649db0cb56c251030e92f8d76ff93
Tony-YI <455054421@qq.com>
2014-02-22 00:07:33 +0800

    testing

--- perl/index.pl
+++ perl/index.pl
@@ -4,16 +4,7 @@ use strict;
 use CGI;	#load the CGI module
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 
-require './lib.pl';
-
 my $CGI_o = CGI->new;
 
-if(cookie_check($CGI_o) eq 0) #cookie is invalid
-{
-    #redirect
-    print $CGI_o->redirect('./login.cgi');
-}
-else
-{
-    print $CGI_o->redirect('./display_panel.html');
-}
+#redirect
+print $CGI_o->redirect('./login.cgi');
