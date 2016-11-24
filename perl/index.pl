3af3a1c53173062ba521e022b1f5e989f3013d41
tony-yi <455054421@qq.com>
2016-11-24 19:44:33 +0800

    modify lib.pl

--- perl/index.pl
+++ perl/index.pl
@@ -6,15 +6,5 @@ use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 
 my $CGI_o = CGI->new;
 
-print <<__html_file__;
-<html>
-   <body>
-       <title>LogIn Interface</title>
-       <p>LogIn Interface</p>
-   </body>
-</html>
-__html_file__
-#when a .cgi file is end with ___html_file__, a new line should be added. Otherwise, error.
-
 #redirect
-#print $CGI_o->redirect('./login.cgi');
+print $CGI_o->redirect('./login.cgi');
